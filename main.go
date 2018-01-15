5df343e05b76ca6655b4134789a22de8ef5aae03
Alina Mackenzie <helloalimac@gmail.com>
2018-01-14 23:19:39 -0600

    Generate weapons and greetings

--- main.go
+++ main.go
@@ -38,12 +38,65 @@ var suffixes = []string{
 	"shuk",
 }
 
-func randomPrefix() string {
-	return prefixes[rand.Intn(len(prefixes)-1)]
+var greetings = []string{
+	"bubu",
+	"dabu",
+	"for the glory of the warchief",
+	"for the horde",
+	"lok'tar",
+	"my life for the horde",
+	"okidoki",
+	"something need doing?",
+	"swobu",
+	"time for killing",
+	"time to die",
+	"work, work",
+	"yes, chieftain?",
+	"yes, warchief",
+	"zub zub",
+	"zug zug",
 }
 
-func randomSuffix() string {
-	return suffixes[rand.Intn(len(suffixes)-1)]
+var weapons = []string{
+	"arrow",
+	"blade",
+	"bomb",
+	"chain",
+	"grenade",
+	"hammer",
+	"kettle",
+	"knife",
+	"paperweight",
+	"plier",
+	"rake",
+	"scoop",
+	"shovel",
+	"sickle",
+	"thrower",
+	"trowel",
+	"tweezer",
+	"wrench",
+}
+
+var adjectives = []string{
+	"dark",
+	"death",
+	"dismal",
+	"doom",
+	"dreary",
+	"dull",
+	"fire",
+	"gloom",
+	"killing",
+	"murderous",
+	"noxious",
+	"ruthless",
+	"soul",
+	"war",
+}
+
+func getRandom(slice []string) string {
+	return slice[rand.Intn(len(slice)-1)]
 }
 
 func uppercaseFirstLetter(word string) string {
@@ -61,7 +114,21 @@ func lowercaseFirstLetter(word string) string {
 // GenerateName is returns a random Orc name
 func GenerateName() string {
 	rand.Seed(time.Now().UnixNano())
-	prefix := uppercaseFirstLetter(randomPrefix())
-	suffix := lowercaseFirstLetter(randomSuffix())
+	prefix := uppercaseFirstLetter(getRandom(prefixes))
+	suffix := lowercaseFirstLetter(getRandom(suffixes))
 	return prefix + suffix
+}
+
+// GenerateGreeting is returns a random Orc greeting
+func GenerateGreeting() string {
+	rand.Seed(time.Now().UnixNano())
+	return uppercaseFirstLetter(getRandom(greetings))
+}
+
+// GenerateWeapon is returns a random Orc weapon
+func GenerateWeapon() string {
+	rand.Seed(time.Now().UnixNano())
+	adjective := uppercaseFirstLetter(getRandom(adjectives))
+	weapon := uppercaseFirstLetter(getRandom(weapons))
+	return adjective + weapon
 }
